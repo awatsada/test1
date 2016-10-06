07b7c4ded7e4acae3b99c0d3f93995576095c4de
Awatsada Wangklin <Awatsada Wangklin>
2016-10-06 13:36:36 +0700

    first commit

---
+++ resources/views/form.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>File Upload Manager</title>
+    <link rel="stylesheet" href="css/app.css" charset="utf-8">
+  </head>
+  <body>
+    <div class = "container">
+      <h3>File Upload Manager</h3>
+      <form action="">
+        <div class = "form-group">
+
+        <input type="file" class = "form-control" name="fileUpload"></div>
+        
+        <div class = "form-group">
+        <input type="submit" ></div>
+      </form>
+    </div>
+  </body>
+</html>
